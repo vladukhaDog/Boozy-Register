605c948c2b4fa2b62b73cdea590853cb1bb5b3c8
vladukhaDog <vladic.p@gmail.com>
2021-06-17 04:20:24 +0300

    Initial Commit

---
+++ Bozzy Register/Bozzy_RegisterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bozzy_RegisterApp.swift
+//  Bozzy Register
+//
+//  Created by vladukha on 17.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct Bozzy_RegisterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
